b63a762723a2ff2b2c84e1032a996174d8b39c11
maiqamaj <71187949+maiqamaj@users.noreply.github.com>
2021-09-12 22:53:06 +0300

    Customer

---
+++ src/main/java/jo/secondstep/bookshelf/entities/Customer.java
@@ -0,0 +1,54 @@
+package jo.secondstep.bookshelf.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+	@Id
+	@GeneratedValue
+	@Column(name = "customer_id")
+	private int customer_id;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "email")
+	private String email;
+	@Override
+	public String toString() {
+		return "Customer [customer_id=" + customer_id + ", username=" + username + ", password=" + password + ", email="
+				+ email + "]";
+	}
+	public int getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	
+}
