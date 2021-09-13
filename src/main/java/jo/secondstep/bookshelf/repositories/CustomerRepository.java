9b75056ef2914c22506c95fb7b6f7b446ef4a082
maiqamaj <71187949+maiqamaj@users.noreply.github.com>
2021-09-13 17:29:14 +0300

    Delete CustomerRepository.java

--- src/main/java/jo/secondstep/bookshelf/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-
-package jo.secondstep.bookshelf.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import jo.secondstep.bookshelf.entities.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-
-}
