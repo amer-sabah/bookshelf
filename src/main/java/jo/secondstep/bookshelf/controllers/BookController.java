f76b9d19ab42e3e93d22dba1118d2bffcc40ca7a
maiqamaj <maiqamaj952@gmail.com>
2021-09-15 16:14:46 +0300

    change files

--- src/main/java/jo/secondstep/bookshelf/controllers/BookController.java
+++ src/main/java/jo/secondstep/bookshelf/controllers/BookController.java
@@ -2,7 +2,6 @@ package jo.secondstep.bookshelf.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
