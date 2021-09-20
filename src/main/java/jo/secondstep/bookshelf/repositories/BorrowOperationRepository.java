6cf7cf198ce1a6c775c93231deddbe26e761ecab
maiqamaj <maiqamaj952@gmail.com>
2021-09-20 16:58:45 +0300

    Add Customer History

--- src/main/java/jo/secondstep/bookshelf/repositories/BorrowOperationRepository.java
+++ src/main/java/jo/secondstep/bookshelf/repositories/BorrowOperationRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import jo.secondstep.bookshelf.entities.BorrowOperations;
+import jo.secondstep.bookshelf.entities.BuyOperations;
 
 public interface BorrowOperationRepository extends CrudRepository<BorrowOperations,Integer> {
 
@@ -15,6 +16,12 @@ public interface BorrowOperationRepository extends CrudRepository<BorrowOperatio
 	@Query(value="select * from borrow_operations where id  like %?1%",nativeQuery = true)
 	public BorrowOperations findBorrowOperationsById(int id);
 	
+	@Query(value = "select * from borrow_operations where accept is null",nativeQuery = true)
+	public List<BorrowOperations> findBorrowOperationsByAccept();
+	
 	@Query(value = "select * from borrow_operations where status like %?1%",nativeQuery = true)
 	public List<BorrowOperations> findBorrowOperationsByStatus(String status);
+	
+	@Query(value = "select * from borrow_operations " + "where customer_id like %?1%", nativeQuery = true)
+	List<BorrowOperations> findAllBook(Integer person_id);
 }
