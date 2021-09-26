a93872a38385ad678fdd60aaf2bd3eaaa16e0988
maiqamaj <maiqamaj952@gmail.com>
2021-09-26 20:06:04 +0300

    Add Rest API

--- src/main/java/jo/secondstep/bookshelf/repositories/BooksRepository.java
+++ src/main/java/jo/secondstep/bookshelf/repositories/BooksRepository.java
@@ -6,18 +6,25 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import jo.secondstep.bookshelf.entities.Book;
+import jo.secondstep.bookshelf.entities.BuyOperations;
 
 public interface BooksRepository extends CrudRepository<Book, Integer> {
 
-	@Query(value = "select * from books "
-			+ "where book_name like %?1%",nativeQuery = true)
+	@Query(value = "select * from books " + "where book_name like %?1%", nativeQuery = true)
 	public List<Book> findBooksByName(String bookName);
-	
-	@Query(value = "select * from books ",nativeQuery = true)
+        
+       @Query(value = "select * from books ",nativeQuery = true)
 	public List<Book> findAll();
-	
 	
 	@Query(value = "select * from books "
 			+ "where  book_id like %?1%",nativeQuery = true)
 	public Book findBookById(int id);
+	// RestAPI
+	// @Query(value = "select name,author,price,description,category.name from Book
+	// where name=?1")
+	// public List<Book> findBookByName(String book_name);
+
+	@Query(value = "select * from  books b join categories c on c.category_id=b.category_id where c.category_name =?1 ", nativeQuery = true)
+	public List<Book> findBooksByCaregory(String caregory_name);
+
 }
