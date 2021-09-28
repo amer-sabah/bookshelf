5fe9c39bc9f95de7d5d52614127a500b7e05cdfc
maiqamaj <71187949+maiqamaj@users.noreply.github.com>
2021-09-28 21:06:17 +0300

    Update BuyOperationsController.java

--- src/main/java/jo/secondstep/bookshelf/controllers/BuyOperationsController.java
+++ src/main/java/jo/secondstep/bookshelf/controllers/BuyOperationsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import jo.secondstep.bookshelf.repositories.BookRatingRepository;
 
 import jo.secondstep.bookshelf.entities.BooksLibrary;
 import jo.secondstep.bookshelf.entities.BuyOperations;
